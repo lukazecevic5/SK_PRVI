6e6e2851627e7754e7c7bf9dc287159a7a1d4cda
Luka <Luka@DESKTOP-MJ8AL4V>
2020-11-09 17:55:33 +0100

    json impl

---
+++ JSONImpl/src/main/java/impl/ImportExportJSON.java
@@ -0,0 +1,33 @@
+package impl;
+
+import java.io.File;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class ImportExportJSON {
+
+
+    public List<Object> importFileToObject(String path) throws Exception
+    {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Object> entiteti = objectMapper.readValue(new File(path), new TypeReference<List<Object>>() {
+        });
+
+        return entiteti;
+    }
+
+    public boolean exportObjectToFile(List<Object> entiteti, String path) throws Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(path), entiteti);
+
+
+
+        return true;
+    }
+
+}

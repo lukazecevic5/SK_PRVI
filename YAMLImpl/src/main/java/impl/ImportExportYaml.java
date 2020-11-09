bfc272dd94f736e1bc476b4b3386eabe9e3726f0
Luka <Luka@DESKTOP-MJ8AL4V>
2020-11-09 18:16:15 +0100

    sve pomalo ono moje

---
+++ YAMLImpl/src/main/java/impl/ImportExportYaml.java
@@ -0,0 +1,53 @@
+package impl;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature;
+
+
+//object == entity
+class ImportExportYaml {
+
+    public List<Object> importFileToObject(String path) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        List<Object> entiteti = objectMapper.readValue(new File(path), new TypeReference<List<Object>>() {
+        });
+
+        return entiteti;
+    }
+    boolean exportObjectToFile(List<Object> entiteti, String path) throws Exception{
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(Feature.WRITE_DOC_START_MARKER));
+        String yaml = mapper.writeValueAsString(entiteti);
+        stringToFile(path, yaml);
+
+        return true;
+    }
+    private void stringToFile(String destinationPath, String data) {
+        File file = new File("");
+        // file = new File(file.getAbsolutePath() + destinationPath);
+        file = new File(destinationPath);
+        file.setWritable(true);
+
+        try (FileOutputStream fos = new FileOutputStream(file);
+                BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+            // convert string to byte array
+            byte[] bytes = data.getBytes();
+            // write byte array to file
+            bos.write(bytes);
+            bos.close();
+            fos.close();
+            System.out.print("Data written to file successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
